79724db05f1bca0069334cc18fd5a02d1a12b4b2
Renato Saito <renato.saito@live.com>
2013-07-21 01:02:19 -0300

    Atualização

--- talks/talks.js
+++ talks/talks.js
@@ -1,13 +1,12 @@
 function TalksViewModel(items) {
-    this.talk = ko.observableArray(items);
-
-    this.showLightningTalks = ko.observable(true);
-    this.showTalks = ko.observable(true);
+	this.talk = ko.observableArray(items);
+	this.showLightningTalks = ko.observable(true);
+	this.showTalks = ko.observable(true);
 };
 
 window.fbAsyncInit = function() {
-    FB.init({appId: 622586384427029, status: true, cookie: true, xfbml: true});
-    $.ajax({
+	FB.init({appId: 622586384427029, status: true, cookie: true, xfbml: true});
+	$.ajax({
 		url: "talks.json?callback=?",
 		dataType: "jsonp",
 		jsonpCallback: "devday2013",
