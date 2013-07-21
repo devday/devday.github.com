c983a1a5f3f63f0b91c963c09dabba5c44d1795e
Renato Saito <renato.saito@live.com>
2013-07-20 22:59:12 -0300

    talks

---
+++ talks/talks.js
@@ -0,0 +1,20 @@
+function TalksViewModel(items) {
+    this.talk = ko.observableArray(items);
+
+    this.showLightningTalks = ko.observable(true);
+    this.showTalks = ko.observable(true);
+};
+
+$.ajax({
+	url: "talks.json?callback=?",
+	dataType: "jsonp",
+	jsonpCallback: "devday2013",
+	cache: true,
+	success: function(data){
+		console.log(data);
+		ko.applyBindings(new TalksViewModel(data));
+	},
+	error: function() {
+		alert("Data not found");
+	}
+});
